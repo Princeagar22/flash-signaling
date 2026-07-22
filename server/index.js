7a38384ff805d8c4fd5ac65f32d964cc776f827c
Princeagar22 <107335301+Princeagar22@users.noreply.github.com>
2026-07-22 15:34:49 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -332,8 +332,8 @@ function publicUser(u) {
 
 function cors(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-flash-admin');
 }
 
 function readJson(req) {
